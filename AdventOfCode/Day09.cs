ab7fdb229b366ab1ca81c40bb6174bd6511a22c9
Rafael Zuolo Coppini Lima <rafaelzcl@gmail.com>
2023-12-09 17:41:18 -0300

    Finished day09 part1 and 2

---
+++ AdventOfCode/Day09.cs
@@ -0,0 +1,79 @@
+﻿
+namespace AdventOfCode;
+
+internal class Day09 : BaseDay
+{
+    private IList<string> input;
+    private IList<long[]> history;
+
+    public Day09()
+    {
+        input = File.ReadAllText(InputFilePath).Split(
+            "\r\n",
+            StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        history = input.Select(line => line.Split(' ').Select(x => long.Parse(x)).ToArray()).ToList();
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var extrapolatedValues = history.Select(ExtrapolateValue);
+
+        return new(extrapolatedValues.Sum().ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var extrapolatedValues = history.Select(ExtrapolatePreviousValue);
+
+        return new(extrapolatedValues.Sum().ToString());
+    }
+
+    private long[] GetDiscreteDerivative(long[] history)
+    {
+        var derivative = new long[history.Length - 1];
+        for (int i = 0; i < history.Length - 1; i++)
+        {
+            derivative[i] = history[i + 1] - history[i];
+        }
+
+        return derivative;
+    }
+
+    private long ExtrapolateValue(long[] history)
+    {
+        var derivativeList = new List<long[]> { history };
+        while (!derivativeList.Last().All(value => value == 0)) 
+        {
+            derivativeList.Add(GetDiscreteDerivative(derivativeList.Last()));
+        }
+
+        var extrapolatedValues = new long[derivativeList.Count];
+
+        for (int i = derivativeList.Count - 2; i >= 0; i--)
+        {
+            var historyToExtrapolate = derivativeList.ElementAt(i);
+            extrapolatedValues[i] = extrapolatedValues[i + 1] + historyToExtrapolate.Last();
+        }
+
+        return extrapolatedValues[0];
+    }
+
+    private long ExtrapolatePreviousValue(long[] history)
+    {
+        var derivativeList = new List<long[]> { history };
+        while (!derivativeList.Last().All(value => value == 0))
+        {
+            derivativeList.Add(GetDiscreteDerivative(derivativeList.Last()));
+        }
+
+        var extrapolatedValues = new long[derivativeList.Count];
+
+        for (int i = derivativeList.Count - 2; i >= 0; i--)
+        {
+            var historyToExtrapolate = derivativeList.ElementAt(i);
+            extrapolatedValues[i] = historyToExtrapolate.First() - extrapolatedValues[i + 1];
+        }
+
+        return extrapolatedValues[0];
+    }
+}
